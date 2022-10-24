872f33340585df44940192ae3417bd9b5af60a18
Stylomines <sylvain_mothes@yahoo.fr>
2022-10-24 12:23:29 +0200

    updating files and folders

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import openpyxl
 import pandas as pd
 import plotly.express as px 
 import plotly.graph_objects as go
 import numpy as np
+
 
 ### CONFIG
 st.set_page_config(
